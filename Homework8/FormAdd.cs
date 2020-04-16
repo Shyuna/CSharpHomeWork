bce94ef7c6ca8c421a0ea665d1af7dfb15258c25
Shyuna <1229324428@qq.com>
2020-04-16 18:13:46 +0800

    homework8

---
+++ Homework8/FormAdd.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Homework6.Program;
+
+namespace OrderManagement
+{
+    public partial class FormAdd : Form
+    {
+        public Order CurrentOrder { get; set; }
+
+        public FormAdd()
+        {
+            InitializeComponent();
+
+        }
+
+        public FormAdd(Order order, bool editMode = false) : this()
+        {
+            CurrentOrder = order;
+            orderBindingSource.DataSource = CurrentOrder;
+            txtID.Enabled = !editMode;
+            if (!editMode)
+            {
+                CurrentOrder.Customer = txtCuName.Text ;
+                CurrentOrder.Address = txtAddr.Text;
+            }
+        }
+
+        private void BtnAddItem_Click(object sender, EventArgs e)
+        {
+            FormEdit formEdit = new FormEdit(new OrderItem());
+            try
+            {
+                if (formEdit.ShowDialog() == DialogResult.OK)
+                {
+                    CurrentOrder.AddItem(formEdit.orderItem);
+                    ItembindingSource1.ResetBindings(false);
+                }
+            }
+            catch (Exception e2)
+            {
+                MessageBox.Show(e2.Message);
+            }
+        }
+
+        private void BtnModifyItem_Click(object sender, EventArgs e)
+        {
+            OrderItem orderItem = ItembindingSource1.Current as OrderItem;
+            if (orderItem == null)
+            {
+                MessageBox.Show("请选择一个订单项进行修改");
+                return;
+            }
+            FormEdit formItemEdit = new FormEdit(orderItem);
+            if (formItemEdit.ShowDialog() == DialogResult.OK)
+            {
+                ItembindingSource1.ResetBindings(false);
+            }
+        }
+
+        private void BtnDeleteItem_Click(object sender, EventArgs e)
+        {
+            OrderItem orderItem = ItembindingSource1.Current as OrderItem;
+            if (orderItem == null)
+            {
+                MessageBox.Show("请选择一个订单项进行删除");
+                return;
+            }
+            CurrentOrder.RemoveItem(orderItem);
+            ItembindingSource1.ResetBindings(false);
+        }
+    }
+}
---
+++ Homework8/FormEdit.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace OrderManagement
+{
+    partial class FormEdit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lblItemNam = new System.Windows.Forms.Label();
+            this.lblNum = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.itembindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.itembindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40.85173F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 59.14827F));
+            this.tableLayoutPanel1.Controls.Add(this.lblItemNam, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblNum, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblPrice, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.txtName, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtNum, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txtPrice, 1, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.33113F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 49.66887F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(634, 151);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // lblItemNam
+            // 
+            this.lblItemNam.AutoSize = true;
+            this.lblItemNam.Location = new System.Drawing.Point(160, 15);
+            this.lblItemNam.Margin = new System.Windows.Forms.Padding(160, 15, 20, 10);
+            this.lblItemNam.Name = "lblItemNam";
+            this.lblItemNam.Size = new System.Drawing.Size(44, 18);
+            this.lblItemNam.TabIndex = 0;
+            this.lblItemNam.Text = "货物";
+            // 
+            // lblNum
+            // 
+            this.lblNum.AutoSize = true;
+            this.lblNum.Location = new System.Drawing.Point(160, 65);
+            this.lblNum.Margin = new System.Windows.Forms.Padding(160, 15, 20, 10);
+            this.lblNum.Name = "lblNum";
+            this.lblNum.Size = new System.Drawing.Size(44, 18);
+            this.lblNum.TabIndex = 1;
+            this.lblNum.Text = "数量";
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Location = new System.Drawing.Point(160, 115);
+            this.lblPrice.Margin = new System.Windows.Forms.Padding(160, 15, 20, 10);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(44, 18);
+            this.lblPrice.TabIndex = 2;
+            this.lblPrice.Text = "单价";
+            // 
+            // txtName
+            // 
+            this.txtName.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.itembindingSource1, "Name", true));
+            this.txtName.Location = new System.Drawing.Point(278, 13);
+            this.txtName.Margin = new System.Windows.Forms.Padding(20, 13, 20, 10);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(182, 28);
+            this.txtName.TabIndex = 3;
+            // 
+            // txtNum
+            // 
+            this.txtNum.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.itembindingSource1, "Amount", true));
+            this.txtNum.Location = new System.Drawing.Point(278, 63);
+            this.txtNum.Margin = new System.Windows.Forms.Padding(20, 13, 20, 10);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(182, 28);
+            this.txtNum.TabIndex = 4;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.itembindingSource1, "Price", true));
+            this.txtPrice.Location = new System.Drawing.Point(278, 113);
+            this.txtPrice.Margin = new System.Windows.Forms.Padding(20, 13, 20, 10);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(182, 28);
+            this.txtPrice.TabIndex = 5;
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOK.Location = new System.Drawing.Point(282, 198);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 30);
+            this.btnOK.TabIndex = 1;
+            this.btnOK.Text = "确定";
+            this.btnOK.UseVisualStyleBackColor = true;
+            // 
+            // itembindingSource1
+            // 
+            this.itembindingSource1.DataSource = typeof(Homework6.Program.OrderItem);
+            // 
+            // FormEdit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 260);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "FormEdit";
+            this.Text = "FormEdit";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.itembindingSource1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label lblItemNam;
+        private System.Windows.Forms.Label lblNum;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.BindingSource itembindingSource1;
+    }
+}
